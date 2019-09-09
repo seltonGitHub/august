1227c1fd483c89de4adeaaacfcba32aa31976490
Selton <seltonzyf@outlook.com>
2019-09-09 17:42:48 +0800

    enhancement #1 完成pojo.

---
+++ pojo/src/main/java/org/itszt/entity/Goods.java
@@ -0,0 +1,18 @@
+package org.itszt.entity;
+
+public class Goods {
+
+    private String price;
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public Goods(String price) {
+        this.price = price;
+    }
+}
---
+++ pojo/src/main/java/org/itszt/inters/IGoodsService.java
@@ -0,0 +1,7 @@
+package org.itszt.inters;
+
+import org.itszt.entity.Goods;
+
+public interface IGoodsService {
+   Goods getGoods();
+}
